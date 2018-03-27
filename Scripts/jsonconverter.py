3bef9ff2959cd44db9a463b94923cab9de40b2ec
David <flio034@gmail.com>
2018-03-27 09:48:47 -0500

    Modificacion script para convertir archivos scv a json

--- Scripts/jsonconverter.py
+++ Scripts/jsonconverter.py
@@ -2,26 +2,13 @@ import csv
 import sys 
 import json
 
-# with open (sys.argv[1],'rb') as f:
-#     reader = csv.DictReader(f)
-#     rows = list(reader)
-#with open('test.json','w') as f:
-#    json.dump(rows,f)
-# def read_csv(c_file,json_file):
-#     csv_row = []
-#     with open(c_file) as csvFile:
-#         reader = csv.DictReader(csvFile)
-#         title = reader.fieldnames
-#         for row in reader:
-#             csv_row.extend([{title[i]:row[title[i]]for i in range(len(title))}])
-#         write_json(csv_row,json_file)
+name = sys.argv[1]
+name = name.split(".")
+name = name[0]
 
-# def write_json(csv_file,json_file):
-#     with open(json_file,'w') as f:
-#         f.write(json.dumps(csv_file))
 with open(sys.argv[1],'rb') as f:
     reader = csv.DictReader(f)
     rows = list(reader)
 
-with open('movie_titles.json', 'w') as f:
-    json.dump(rows, f)
+with open(name + '.json', 'w') as f:
+     json.dump(rows, f)
